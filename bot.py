c71b8bf372d1f697b0c2edab2322acc12de9cdaf
shelby <faulconershelby@gmail.com>
2023-04-11 20:09:25 -0700

    added new dependencies, added dice command. still not able to connect to server?

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import os
+import random
+from twitchio import Message
 from twitchio.ext import commands
+# from twitchio.client import Client
+from twitchio.errors import TwitchIOException
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,15 +18,52 @@ bot = commands.Bot(
     client_secret=os.environ['TWITCH_CLIENT_SECRET'],
     initial_channels=[os.environ['CHANNEL']]
 )
+# opts = {
+#     'identity': {
+#     'username': os.environ['BOT_NICK'],
+#     'password:': os.environ['TMI_TOKEN'],
+#     },
+#     'channels': [os.environ['CHANNEL']] # type: ignore
+# }
+
+# client = Client(**opts)
+
+@bot.event #type: ignore
+async def on_message_handler(channel: str, message: Message) -> None:
+  if message.author.name.lower() == os.environ['BOT_NICK'].lower():
+      return
+  
+  command_name = message.content.strip()
+
+  if command_name == (f'!dice'):
+    num = roll_dice()
+    await message.channel.send(f'@{message.author.name} rolled a {num}')
+    print(f'@{message.author.name} executed {command_name}')
+  else:
+    print(f'unknown command: {command_name}')
 
 @bot.event # type: ignore
-async def event_ready():
-    'Called once when the bot goes online.'
+async def on_connect_handler():
     print(f'{os.environ["BOT_NICK"]} is online!')
-    ws = bot._ws # type: ignore
-    await ws.send_msg(os.environ['CHANNEL'], f"/me has arrived, beep boop!")
+    # ws = bot._ws # type: ignore
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has arrived, beep boop!")
 
-    
+
+def roll_dice() -> int:
+    sides = 20
+    return random.randint(1, sides)
+
+bot.event(on_connect_handler) # type: ignore
+bot.event(on_message_handler) # type: ignore
+
+# client.event(on_connect_handler) # type: ignore
+# client.event(on_message_handler) #type: ignore
+
+
 #bot.py
-if __name__ == "__main__":
-    bot.run()
+try:
+    # client.run()
+    if __name__ == "__main__":
+      bot.run()
+except TwitchIOException as e:
+    print(e)
