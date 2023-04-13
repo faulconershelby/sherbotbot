beef0df60653f00fd362680b3912319c4905a4cf
shelby <faulconershelby@gmail.com>
2023-04-12 23:33:11 -0700

    added additional commands. bot works

--- bot.py
+++ bot.py
@@ -2,68 +2,75 @@ import os
 import random
 from twitchio import Message
 from twitchio.ext import commands
-# from twitchio.client import Client
 from twitchio.errors import TwitchIOException
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 bot = commands.Bot(
     # set up the bot
-    irc_token=os.environ['TMI_TOKEN'],
-    client_id=os.environ['CLIENT_ID'],
-    nick=os.environ['BOT_NICK'],
+    irc_token=os.getenv('TMI_TOKEN'),
+    client_id=os.getenv('CLIENT_ID'),
+    nick=os.getenv('BOT_NICK'),
     token=os.environ['BOT_TOKEN'],
     prefix=os.environ['BOT_PREFIX'],
     client_secret=os.environ['TWITCH_CLIENT_SECRET'],
     initial_channels=[os.environ['CHANNEL']]
 )
-# opts = {
-#     'identity': {
-#     'username': os.environ['BOT_NICK'],
-#     'password:': os.environ['TMI_TOKEN'],
-#     },
-#     'channels': [os.environ['CHANNEL']] # type: ignore
-# }
 
-# client = Client(**opts)
-
-@bot.event #type: ignore
+@bot.event  # type: ignore
 async def on_message_handler(channel: str, message: Message) -> None:
-  if message.author.name.lower() == os.environ['BOT_NICK'].lower():
-      return
-  
-  command_name = message.content.strip()
+  assert message.author.name is not None
+  assert message.content is not None
 
-  if command_name == (f'!dice'):
-    num = roll_dice()
-    await message.channel.send(f'@{message.author.name} rolled a {num}')
-    print(f'@{message.author.name} executed {command_name}')
-  else:
-    print(f'unknown command: {command_name}')
+  if message.author.name.lower() == os.environ['BOT_NICK'].lower():
+    return
+  command_name = message.content.strip()
+  
+  await bot.handle_commands(message)
+
+  print(f'@{message.author.name} executed {command_name}')
 
 @bot.event # type: ignore
-async def on_connect_handler():
-    print(f'{os.environ["BOT_NICK"]} is online!')
-    # ws = bot._ws # type: ignore
+async def event_ready():
+    print(f'{os.environ["BOT_NICK"]} || online')
+    print(f'user id || {bot.user_id}')
+
+    ws = bot._ws # type: ignore
+
     await ws.send_privmsg(os.environ['CHANNEL'], f"/me has arrived, beep boop!")
 
+@bot.command(name='hello')
+async def say_hello(message):
+    await message.send(f'Hello, {message.author.name} <3!')
 
-def roll_dice() -> int:
-    sides = 20
-    return random.randint(1, sides)
+@bot.command(name='dice')
+async def roll_dice(message):
+    # remove characters from the message that aren't numbers
+    if message.isalpha() is False:
+      sides = int(''.join([i for i in message.content if i.isdigit()]))
+    else:
+      sides = 6
+    await message.send(f' {message.author.name} rolled {random.randint(1, sides)}')
 
-bot.event(on_connect_handler) # type: ignore
+@bot.command(name='subraid')
+async def subraid(message):
+  await message.send("sherbo4Figaroscreams sherbo4Catscream sherbo4Awoooosherbo4Hype :sparkles: :lotus: sherbo4Love MercyWing1 sherbo4Pinocchio SHERBOT :rotating_light: RAID sherbo4Pinocchio MercyWing2 sherbo4Love :lotus: :sparkles: sherbo4Hype sherbo4Awoooo sherbo4Catscream sherbo4Figaroscreams")
+
+@bot.command(name='test')
+async def test(message):
+  await message.send('test passed!')
+
+bot.event(event_ready) # type: ignore
 bot.event(on_message_handler) # type: ignore
-
-# client.event(on_connect_handler) # type: ignore
-# client.event(on_message_handler) #type: ignore
-
+bot.command(say_hello) # type: ignore
+bot.command(roll_dice) # type: ignore
+bot.command(subraid) # type: ignore
+bot.command(test) # type: ignore
 
 #bot.py
 try:
-    # client.run()
     if __name__ == "__main__":
-      bot.run()
+        bot.run()
 except TwitchIOException as e:
     print(e)
