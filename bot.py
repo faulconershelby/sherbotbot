d0a3a32efba906c3265d7dcb95845af66e5838b0
shelby <faulconershelby@gmail.com>
2023-04-21 10:19:12 -0700

    users can now draw as many cards as they want for the tarot deck, well up to 78

--- bot.py
+++ bot.py
@@ -199,7 +199,17 @@ async def sun_sign_command(message):
 async def draw_card(message):
   tarot_deck = [  "The Fool",  "The Magician",  "The High Priestess",  "The Empress",  "The Emperor",  "The Hierophant",  "The Lovers",  "The Chariot",  "Strength",  "The Hermit",  "Wheel of Fortune",  "Justice",  "The Hanged Man",  "Death",  "Temperance",  "The Devil",  "The Tower",  "The Star",  "The Moon",  "The Sun",  "Judgement",  "The World",  "Ace of Wands",  "Two of Wands",  "Three of Wands",  "Four of Wands",  "Five of Wands",  "Six of Wands",  "Seven of Wands",  "Eight of Wands",  "Nine of Wands",  "Ten of Wands",  "Page of Wands",  "Knight of Wands",  "Queen of Wands",  "King of Wands",  "Ace of Cups",  "Two of Cups",  "Three of Cups",  "Four of Cups",  "Five of Cups",  "Six of Cups",  "Seven of Cups",  "Eight of Cups",  "Nine of Cups",  "Ten of Cups",  "Page of Cups",  "Knight of Cups",  "Queen of Cups",  "King of Cups",  "Ace of Swords",  "Two of Swords",  "Three of Swords",  "Four of Swords",  "Five of Swords",  "Six of Swords",  "Seven of Swords",  "Eight of Swords",  "Nine of Swords",  "Ten of Swords",  "Page of Swords",  "Knight of Swords",  "Queen of Swords",  "King of Swords",  "Ace of Pentacles",  "Two of Pentacles",  "Three of Pentacles",  "Four of Pentacles",  "Five of Pentacles",  "Six of Pentacles",  "Seven of Pentacles",  "Eight of Pentacles",  "Nine of Pentacles",  "Ten of Pentacles",  "Page of Pentacles",  "Knight of Pentacles",  "Queen of Pentacles",  "King of Pentacles"]
   card = random.choice(tarot_deck)
-  await message.send(f'{message.author.name} drew {card}')
+  hand = []
+  try:
+    num_cards = int(message.message.content.split()[1])
+    if num_cards < 1 or num_cards > 78:
+      raise ValueError
+  except (ValueError, IndexError):
+    num_cards = 1
+  for i in range(num_cards):
+    card = random.choice(tarot_deck)
+    hand.append(card)
+  await message.send(f'{message.author.name} drew {hand}')
   
 
 @bot.command(name='subraid')
