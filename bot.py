7201cf55ede623b1565b94ff1513c4dec4252468
shelby <faulconershelby@gmail.com>
2023-04-20 19:18:58 -0700

    still fixing the asto command, but hey, it's outputting a sign again! added an astro-draw command

--- bot.py
+++ bot.py
@@ -136,49 +136,54 @@ async def sherbot_commands(message):
 # @todo: add a command to tell sun sign of user if they add their birthday
 @bot.command(name= 'sunsign')
 async def sun_sign_command(message):
-
-
   # create a function that takes in a message and returns the user's birthday
   # if the user doesn't have a birthday, return ValueError('valid birthday pls')
   # if the user has a birthday, continue
   def get_birthday(message):
     astrology_signs= {
-      'aries': {'march': range(21, 31), 'april': range(1, 19)},
-      'taurus': {'april': range(20, 30), 'may': range(1, 20)},
-      'gemini': {'may': range(21, 31), 'june': range(1, 20)},
-      'cancer': {'june': range(21, 30), 'july': range(1, 22)},
-      'leo': {'july': range(23, 31), 'august': range(1, 22)},
-      'virgo': {'august': range(23, 31), 'september': range(1, 22)},
-      'libra': {'september': range(23, 30), 'october': range(1, 22)},
-      'scorpio': {'october': range(23, 31), 'november': range(1, 21)},
-      'sagittarius': {'november': range(22, 30), 'december': range(1, 21)},
-      'capricorn': {'december': range(22, 31), 'january': range(1, 19)},
-      'aquarius': {'january': range(20, 31), 'february': range(1, 18)},
-      'pisces': {'february': range(19, 29), 'march': range(1, 20)}
-    }
-    months_list = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+      'march': {'aries': range(21, 31), 'pisces': range(1, 20)},
+      'april': {'taurus': range(20, 30), 'aries': range(1, 19)},
+      'may': {'gemini': range(21, 31), 'taurus': range(1, 20)},
+      'june': {'cancer': range(21, 30), 'gemini': range(1, 20)},
+      'july': {'leo': range(23, 31), 'cancer': range(1, 22)},
+      'august': {'virgo': range(23, 31), 'leo': range(1, 22)},
+      'september': {'libra': range(23, 30), 'virgo': range(1, 22)},
+      'october': {'scorpio': range(23, 31), 'libra': range(1, 22)},
+      'november': {'sagittarius': range(22, 30), 'scorpio': range(1, 21)},
+      'december': {'capricorn': range(22, 31), 'sagittarius': range(1, 21)},
+      'january': {'aquarius': range(20, 31), 'capricorn': range(1, 19)},
+      'february': {'pisces': range(19, 29), 'aquarius': range(1, 18)}
+  }
+    # months_list = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+    message_content = str(message.message.content).lower()
+    words = message_content.split()[1:3]
+    user_month = None
+    user_day = None
+    print('words', words, "message_content", message_content)
 
-    # create a list to hold all elements in message.message
-    # combine all element in list to single string
-    # then search for month and day in string
-    # if month and day are found, store month and day in variables
+    month = words[0]
+    day = words[1]
+    print('month', month, 'day', day)
 
-    for month in months_list:
-      if month in str(message.message.content).lower():
-        for day in range(1, 32):
-          if str(day) in str(message.message.content).lower():
-            user_month = month
-            user_day = int(str(day))
-            print(user_month, user_day)
+    user_month = month.lower().strip()
+    user_day = int(day.strip(','))
+    print('user_month', user_month, 'user_day', user_day)
 
-            for sign, months in astrology_signs.items():
-              if month in months and day in months[month]:
-                user_sign = sign
-                return user_sign
-    raise  Exception('valid birthday pls') 
+    sign_for_month = astrology_signs[user_month]
+
+    # if user_month and user_month not in astrology_signs:
+    #   raise ValueError('invalid month')
+    # if not(user_month and user_day not in astrology_signs[user_month].values()):
+    #   raise ValueError('invalid day')
+
+    for sign_month in astrology_signs.values():
+      for sign, day_range in sign_month.items():
+        if user_month == month and user_day in day_range:
+          print('sign', sign)
+          return sign
+      
   # if user calls sun-sign command, return their sign with get_birthday() function
   # if user calls sun-sign command and doesn't have a birthday, return ValueError('valid birthday pls')
-    
   try:
     user_sign = get_birthday(message)
     if user_sign:
@@ -186,9 +191,16 @@ async def sun_sign_command(message):
   except ValueError as e:
     print(str(e))
 
+@bot.command(name='draw')
+async def draw_card(message):
+  tarot_deck = [  "The Fool",  "The Magician",  "The High Priestess",  "The Empress",  "The Emperor",  "The Hierophant",  "The Lovers",  "The Chariot",  "Strength",  "The Hermit",  "Wheel of Fortune",  "Justice",  "The Hanged Man",  "Death",  "Temperance",  "The Devil",  "The Tower",  "The Star",  "The Moon",  "The Sun",  "Judgement",  "The World",  "Ace of Wands",  "Two of Wands",  "Three of Wands",  "Four of Wands",  "Five of Wands",  "Six of Wands",  "Seven of Wands",  "Eight of Wands",  "Nine of Wands",  "Ten of Wands",  "Page of Wands",  "Knight of Wands",  "Queen of Wands",  "King of Wands",  "Ace of Cups",  "Two of Cups",  "Three of Cups",  "Four of Cups",  "Five of Cups",  "Six of Cups",  "Seven of Cups",  "Eight of Cups",  "Nine of Cups",  "Ten of Cups",  "Page of Cups",  "Knight of Cups",  "Queen of Cups",  "King of Cups",  "Ace of Swords",  "Two of Swords",  "Three of Swords",  "Four of Swords",  "Five of Swords",  "Six of Swords",  "Seven of Swords",  "Eight of Swords",  "Nine of Swords",  "Ten of Swords",  "Page of Swords",  "Knight of Swords",  "Queen of Swords",  "King of Swords",  "Ace of Pentacles",  "Two of Pentacles",  "Three of Pentacles",  "Four of Pentacles",  "Five of Pentacles",  "Six of Pentacles",  "Seven of Pentacles",  "Eight of Pentacles",  "Nine of Pentacles",  "Ten of Pentacles",  "Page of Pentacles",  "Knight of Pentacles",  "Queen of Pentacles",  "King of Pentacles"]
+  card = random.choice(tarot_deck)
+  await message.send(f'{message.author.name} drew {card}')
+  
+
 @bot.command(name='subraid')
 async def subraid(message):
-  await message.send("sherbo4Figaroscreams sherbo4Catscream sherbo4Awoooo sherbo4Hype :sparkles: :lotus: sherbo4Love MercyWing1 sherbo4Pinocchio SHERBOT :rotating_light: RAID sherbo4Pinocchio MercyWing2 sherbo4Love :lotus: :sparkles: sherbo4Hype sherbo4Awoooo sherbo4Catscream sherbo4Figaroscreams")
+  await message.send("sherbo4FigSaroscreams sherbo4Catscream sherbo4Awoooo sherbo4Hype :sparkles: :lotus: sherbo4Love MercyWing1 sherbo4Pinocchio SHERBOT :rotating_light: RAID sherbo4Pinocchio MercyWing2 sherbo4Love :lotus: :sparkles: sherbo4Hype sherbo4Awoooo sherbo4Catscream sherbo4Figaroscreams")
 
 @bot.command(name="pinocchio")
 async def pinocchio_emote(message):
