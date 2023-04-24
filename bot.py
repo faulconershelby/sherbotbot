064a917c09d0e114dd2f3569864f499de8082c1a
shelby <faulconershelby@gmail.com>
2023-04-23 22:17:58 -0700

    had to fix subraid command

--- bot.py
+++ bot.py
@@ -211,10 +211,15 @@ async def draw_card(message):
     hand.append(card)
   await message.send(f'{message.author.name} drew {hand}')
   
+@bot.command(name='8ball')
+async def eight_ball(message):
+  eight_ball_answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+  answer = random.choice(eight_ball_answers)
+  await message.send(f'Magic 8-ball says: {answer}')
 
 @bot.command(name='subraid')
 async def subraid(message):
-  await message.send("sherbo4FigSaroscreams sherbo4Catscream sherbo4Awoooo sherbo4Hype :sparkles: :lotus: sherbo4Love MercyWing1 sherbo4Pinocchio SHERBOT :rotating_light: RAID sherbo4Pinocchio MercyWing2 sherbo4Love :lotus: :sparkles: sherbo4Hype sherbo4Awoooo sherbo4Catscream sherbo4Figaroscreams")
+  await message.send("sherbo4Figaroscreams sherbo4Catscream sherbo4Awoooo sherbo4Hype :sparkles: :lotus: sherbo4Love MercyWing1 sherbo4Pinocchio SHERBOT :rotating_light: RAID sherbo4Pinocchio MercyWing2 sherbo4Love :lotus: :sparkles: sherbo4Hype sherbo4Awoooo sherbo4Catscream sherbo4Figaroscreams")
 
 @bot.command(name="pinocchio")
 async def pinocchio_emote(message):
